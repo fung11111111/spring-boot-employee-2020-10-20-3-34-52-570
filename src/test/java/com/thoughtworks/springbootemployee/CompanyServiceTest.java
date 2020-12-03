dac2c61c1f628ad57e05ddb96714505873c3e6eb
Ray Lam <ray.lam@oocl.com>
2020-12-03 13:27:30 +0800

    [SpringBoot Company][Ray] refactor: inject company service and mock companyRepository

--- src/test/java/com/thoughtworks/springbootemployee/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyServiceTest.java
@@ -4,9 +4,15 @@ import com.thoughtworks.springbootemployee.Exception.CompanyNotFoundException;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import com.thoughtworks.springbootemployee.service.CompanyService;
+import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,13 +22,18 @@ import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class CompanyServiceTest {
+    @InjectMocks
+    private CompanyService companyService;
+
+    @Mock
+    private CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
+
 
     @Test
     public void should_return_all_companies_when_get_companies_given_repository_with_all_companies() {
         //given
-        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
-        CompanyService companyService = new CompanyService(companyRepository);
         List<Company> expectedCompanies = Arrays.asList(new Company());
         when(companyRepository.getCompanies()).thenReturn(expectedCompanies);
 
@@ -36,8 +47,6 @@ public class CompanyServiceTest {
     @Test
     public void should_add_company_when_add_company_given_repository_with_company() {
         //given
-        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
-        CompanyService companyService = new CompanyService(companyRepository);
         Company expectedCompany = new Company();
         when(companyRepository.addCompany(expectedCompany)).thenReturn(expectedCompany);
 
@@ -51,8 +60,6 @@ public class CompanyServiceTest {
     @Test
     public void should_company_when_get_company_by_id_given_repository_with_company_id() throws CompanyNotFoundException {
         //given
-        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
-        CompanyService companyService = new CompanyService(companyRepository);
         Company expectedCompany = new Company(123, "Hi", "Banking", new ArrayList<>());
         when(companyRepository.getCompanyById(123)).thenReturn(expectedCompany);
 
@@ -66,11 +73,7 @@ public class CompanyServiceTest {
     @Test
     public void should_return_updated_company_when_update_company_given_repository_with_company_and_company_id() throws CompanyNotFoundException {
         //given
-        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
-        CompanyService companyService = new CompanyService(companyRepository);
-        Company existingCompany = new Company(123, "HI COM", "Banking", new ArrayList<>());
         Company expectedCompany = new Company(123, "Hello", "Banking", new ArrayList<>());
-        when(companyRepository.addCompany(existingCompany)).thenReturn(existingCompany);
         when(companyRepository.updateCompany(123, expectedCompany)).thenReturn(expectedCompany);
 
         //when
@@ -83,10 +86,6 @@ public class CompanyServiceTest {
 
     @Test
     public void should_call_deleteCompanyByID_when_service_delete_company_by_id_given_repository_with_company_id() throws CompanyNotFoundException {
-        //given
-        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
-        CompanyService companyService = new CompanyService(companyRepository);
-
         //when
         companyService.deleteCompanyById(1);
 
@@ -98,8 +97,6 @@ public class CompanyServiceTest {
     public void should_return_employees_when_get_employees_by_company_id_given_repository_with_company_id() throws CompanyNotFoundException {
         //given
         //shd only add employee id to company
-        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
-        CompanyService companyService = new CompanyService(companyRepository);
         ArrayList<Employee> expectedEmployees = new ArrayList<>();
         expectedEmployees.add(new Employee());
         expectedEmployees.add(new Employee());
@@ -116,12 +113,9 @@ public class CompanyServiceTest {
     @Test
     void should_return_2_companies_when_get_companies_with_pagination_given_companies_more_than_2_with_pageNumber_is_1_and_pageSize_is_2() {
         //given
-        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
-        CompanyService companyService = new CompanyService(companyRepository);
         List<Company> expectedCompanies = new ArrayList<>();
         expectedCompanies.add(new Company(1, "A COM", "Banking", new ArrayList<>()));
         expectedCompanies.add(new Company(2, "B COM", "Banking", new ArrayList<>()));
-        expectedCompanies.add(new Company(3, "C COM", "Banking", new ArrayList<>()));
         when(companyRepository.getWithPagination(1, 2)).thenReturn(expectedCompanies);
 
         //when
@@ -134,8 +128,6 @@ public class CompanyServiceTest {
     @Test
     void should_throw_company_not_found_exception_when_get_company_by_id_given_not_exist_id() throws CompanyNotFoundException {
         //given
-        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
-        CompanyService companyService = new CompanyService(companyRepository);
         when(companyRepository.getCompanyById(20)).thenThrow(new CompanyNotFoundException());
 
         //when
