868626ee787b7bce34a8cfda5a55d07ed30e03b1
Ray Lam <ray.lam@oocl.com>
2020-12-04 15:22:22 +0800

    [SpringBoot ErrorResponse][Ray] feat: create DTO with employee request and employee response

--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -7,14 +7,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
-
     Optional<Employee> findById(String id);
 
-    Employee save(Employee employee);
-
     List<Employee> findByGender(String gender);
-
-    boolean existsById(String id);
 
     List<Employee> findByCompanyId(String companyId);
 
